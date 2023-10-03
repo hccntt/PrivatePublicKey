75d0d15a448a652e46f1b85bacf625c87df2538c
conh1 <hccntt@gmail.com>
2023-10-03 14:20:18 +0700

    Add project files.

---
+++ PrivatePublicKey/Program.cs
@@ -0,0 +1,97 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PrivatePublicKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = @"Connect with me on linkedin: https://www.linkedin.com/in/dziwoki";
+
+            string cihperText = encrypt(plainText);
+            string decryptedCipherText = decrypt(cihperText);
+
+            Console.WriteLine("Encrypted text: {0}", cihperText);
+            Console.WriteLine("Decrypted text {0}. Encryption/Decryption was correct {1}",
+                decryptedCipherText, (plainText == decryptedCipherText).ToString());
+
+            Console.ReadLine();
+        }
+
+        static string encrypt(string plainText)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            PemReader pr = new PemReader(
+                (StreamReader)File.OpenText("./Key/publicKey.pem")
+            );
+            RsaKeyParameters keys = (RsaKeyParameters)pr.ReadObject();
+
+            // Pure mathematical RSA implementation
+            // RsaEngine eng = new RsaEngine();
+
+            // PKCS1 v1.5 paddings
+            // Pkcs1Encoding eng = new Pkcs1Encoding(new RsaEngine());
+
+            // PKCS1 OAEP paddings
+            OaepEncoding eng = new OaepEncoding(new RsaEngine());
+            eng.Init(true, keys);
+
+            int length = plainTextBytes.Length;
+            int blockSize = eng.GetInputBlockSize();
+            List<byte> cipherTextBytes = new List<byte>();
+            for (int chunkPosition = 0;
+                chunkPosition < length;
+                chunkPosition += blockSize)
+            {
+                int chunkSize = Math.Min(blockSize, length - chunkPosition);
+                cipherTextBytes.AddRange(eng.ProcessBlock(
+                    plainTextBytes, chunkPosition, chunkSize
+                ));
+            }
+            return Convert.ToBase64String(cipherTextBytes.ToArray());
+        }
+
+        static string decrypt(string cipherText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+
+            PemReader pr = new PemReader(
+                (StreamReader)File.OpenText("./Key/privateKey.pem")
+            );
+            AsymmetricCipherKeyPair keys = (AsymmetricCipherKeyPair)pr.ReadObject();
+
+            // Pure mathematical RSA implementation
+            // RsaEngine eng = new RsaEngine();
+
+            // PKCS1 v1.5 paddings
+            // Pkcs1Encoding eng = new Pkcs1Encoding(new RsaEngine());
+
+            // PKCS1 OAEP paddings
+            OaepEncoding eng = new OaepEncoding(new RsaEngine());
+            eng.Init(false, keys.Private);
+
+            int length = cipherTextBytes.Length;
+            int blockSize = eng.GetInputBlockSize();
+            List<byte> plainTextBytes = new List<byte>();
+            for (int chunkPosition = 0;
+                chunkPosition < length;
+                chunkPosition += blockSize)
+            {
+                int chunkSize = Math.Min(blockSize, length - chunkPosition);
+                plainTextBytes.AddRange(eng.ProcessBlock(
+                    cipherTextBytes, chunkPosition, chunkSize
+                ));
+            }
+            return Encoding.UTF8.GetString(plainTextBytes.ToArray());
+        }
+    }
+}
